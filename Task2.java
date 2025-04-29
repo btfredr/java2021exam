b191bff25d7431d827752588e5e0756417834f5c
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-04-29 12:13:18 +0200

    FInished the chess board and fixed the logic

--- Task2.java
+++ Task2.java
@@ -15,10 +15,13 @@ public class Task2 {
         char ltr = 'A';
 
         for (int i = 1; i <= 8; i++) {
+
             for (int j = 1; j <= 8; j++) {
                 System.out.print("" + ltr + i + " ");
                 ltr++;
             }
+            
+            ltr = 'A';
             System.out.println();
         }
     }
