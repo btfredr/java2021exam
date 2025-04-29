92e642767a8e892a7e7facf38334bb6572a69590
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-04-29 11:32:38 +0200

    Task 1 init

---
+++ Task1.java
@@ -0,0 +1,16 @@
+/*
+    Write a method that takes a integer value (x) as an argument and returns smallest odd number
+    bigger than input number x.
+
+    For exemple:
+    For x= 25 the method returns 27
+    For x= 34 the method returns 35
+    For x= -7 the method returns -5
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+        
+    }
+
+}
