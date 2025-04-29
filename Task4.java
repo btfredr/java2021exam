7436561b6f36ae141d7a69fea3b151fac816ae63
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-04-29 20:08:24 +0200

    task 4 init and added scanner

---
+++ Task4.java
@@ -0,0 +1,21 @@
+/*
+    Write a Java program that transforms the temperature (input from user) in Celsius into temperature
+    in Fahrenheit, using the formula f=9*c/5+32
+
+    For example:
+    if c is 0, the method returns 32
+    if c is 10, the method returns 50
+ */
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a Celcius temperature: ");
+
+        
+    }
+
+
+}
