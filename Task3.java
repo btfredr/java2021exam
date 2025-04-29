b8eefa0a99ecc880e73a53f7f133d6d07b6ee5b4
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-04-29 12:22:16 +0200

    added else statement, working on loop now

---
+++ Task3.java
@@ -0,0 +1,25 @@
+/*
+    Write a method that takes integer number n as an argument. After that, it checks if n is odd or even.
+    In the case its even, the program should use a while loop to write 5 subsequent even numbers. In
+    the case it is odd, the program should multiply number n with 25, and write a result.
+    E.g. 1: for n = 6, the program should write: 8,10,12,14,16
+    E.g. 2: for n = 7, the program should write: 175
+ */
+
+
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void oddOrEven(int n) {
+        if (n % 2 == 0) {
+            while(n < 0) {
+                int current = n + 2;
+            }
+        } else {
+            int multiply = n * 25;
+            System.out.print(multiply);
+        }
+    }
+}
