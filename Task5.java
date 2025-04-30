08e994f0d2c5866d8adcb136ff391afee491f156
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-04-30 12:01:53 +0200

    added variables to keep track of letters and digits

--- Task5.java
+++ Task5.java
@@ -2,16 +2,20 @@
     Write a Java program that shows a message about number of digits and letters in a String using the
     method:
     public void printNumberOfDigitsAndLetters(String input){}
-    
+
     E.g. 1: if the input is: “I am 25 years old”, the method should print:
     The line contains 2 digits and 11 letters!
  */
 
+import java.util.Scanner;
 
 public class Task5 {
-    
 
     public void printNumbersOfDigitsAndLetters(String input) {
-
+        int digitCount = 0;
+        int letterCount = 0;
+        
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextLine();
     }
 }
