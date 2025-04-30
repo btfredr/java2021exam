d3952271f3c76d6a7f640207aa172b8d5cd40938
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-04-30 18:22:15 +0200

    made instances of subclasses

--- Task6/Advisors.java
+++ Task6/Advisors.java
@@ -1,5 +1,35 @@
 package Task6;
 
-public class Advisors {
-    
+/*
+     9. Advisors subclass should contain additional field that stores the name of the department
+    (that advisor is employed by) (2 points).
+
+    10. Use polymorphism to create two methods in Advisors class to print information about the
+    advisors. One method should print only first name and last name. Other method should
+    print all available information. (4 points).
+
+ */
+
+public class Advisors extends Employees {
+    public String department;
+
+    public Advisors(int number, String firstName, String lastName, int age, String department) {
+        super(number, firstName, lastName, age); 
+        this.department = department;
+    }
+
+    public void printName() {
+        System.out.println("Advisor info:");
+        System.out.println("First Name: " + getFirstName());
+        System.out.print("Last Name: " + getFirstName());
+    }
+
+    public void printAllInfo() {
+        System.out.println("Advisor info:");
+        System.out.println("Employee Number: " + getEmpNum());
+        System.out.println("First Name: " + getFirstName());
+        System.out.print("Last Name: " + getFirstName());
+        System.out.print("Age: " + getAge());
+        System.out.println("Department: " + department);
+    }
 }
---
+++ Task6/Test.java
@@ -0,0 +1,14 @@
+package Task6;
+/*
+    11. Create simple Test class with main method, that will create instances of one Professor and
+    one Advisor and print details from both (5 points).
+ */
+public class Test {
+    
+    public static void main(String[] args) {
+        Professors prof = new Professors(1, "Leon", "Hoff", 24, "CS", true);
+        Advisors adv = new Advisors(1, "Geir", "Hagen", 45, "Economics");
+
+        
+    }
+}
