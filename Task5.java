ce1d47ebec827013e521ad581c505df7eb3c7dbb
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-04-30 11:53:46 +0200

    starting to make  method

---
+++ Task5.java
@@ -0,0 +1,17 @@
+/*
+    Write a Java program that shows a message about number of digits and letters in a String using the
+    method:
+    public void printNumberOfDigitsAndLetters(String input){}
+    
+    E.g. 1: if the input is: “I am 25 years old”, the method should print:
+    The line contains 2 digits and 11 letters!
+ */
+
+
+public class Task5 {
+    
+
+    public void printNumbersOfDigitsAndLetters(String input) {
+
+    }
+}
